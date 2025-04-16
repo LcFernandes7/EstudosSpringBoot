797cdeac846530c60aa4cd4f6d717852c2d1b561
Lucas de Oliveira Fernandes <lucas.fernandes@BTNWAWS.CORP>
2025-04-15 23:26:02 -0300

    Adicionando o projeto Spring Boot

---
+++ api/src/test/java/med/voll/api/ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package med.voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootApplication
+class ApiApplicationTests {
+
+	public static void main(String[] args){
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
